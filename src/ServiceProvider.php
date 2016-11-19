0f932c4b91d5ae11d9d82d5d17deb69efc7dd65e
Till Backhaus <till@backha.us>
2016-11-19 16:14:31 +0100

    Implement preliminary redis adapter. (#3)

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,11 +4,16 @@ namespace Tback\PrometheusExporter;
 
 use Prometheus\CollectorRegistry;
 use Prometheus\Storage\APC;
+use Prometheus\Storage\Redis;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/config.php' => config_path('prometheus_exporter.php'),
+        ]);
+
         if (!$this->app->routesAreCached()) {
             require __DIR__.'/routes.php';
         }
@@ -16,8 +21,22 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     public function register()
     {
+        $this->mergeConfigFrom(
+            __DIR__.'/config.php', 'prometheus_exporter'
+        );
+
         $this->app->singleton('prometheus', function ($app) {
-            $adapter = new APC();
+            switch (config('prometheus_exporter.adapter')) {
+                case 'apc':
+                    $adapter = new APC();
+                    break;
+                case 'redis':
+                    $adapter = new Redis(config('prometheus_exporter.redis'));
+                    break;
+                default:
+                    throw new \ErrorException('"prometheus_exporter.adapter" must be either apc or redis');
+            }
+
             $registry = new CollectorRegistry($adapter);
 
             return $registry;
---
+++ src/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+     * possible values are apc or redis
+     */
+    'adapter' => env('PROMETHEUS_ADAPTER', 'apc'),
+
+    'name' => 'http_server',
+
+    'redis' => [
+        'host'                   => '127.0.0.1',
+        'port'                   => 6379,
+        'timeout'                => 0.1,  // in seconds
+        'read_timeout'           => 10, // in seconds
+        'persistent_connections' => false,
+    ],
+];
